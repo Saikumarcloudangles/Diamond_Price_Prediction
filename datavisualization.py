b093caa9558a18ee2458b49364ce950d5db8585b
Saikumarcloudangles <146807683+Saikumarcloudangles@users.noreply.github.com>
2023-12-05 23:40:36 +0530

    Add files via upload

---
+++ datavisualization.py
@@ -0,0 +1,48 @@
+from data_preprocessing import data_preprocessing
+import plotly.express as px
+import warnings
+warnings.filterwarnings("ignore")
+import plotly.figure_factory as ff
+import string
+
+
+def data_visualization():
+
+    data=data_preprocessing()
+    count = 0
+    letters = list(string.ascii_lowercase)
+    names_cat=["cut","color","clarity"]
+    for i in names_cat:
+        count+=1
+        lst = []
+        lst.append(i)
+        df = data.groupby(by=lst).size().reset_index(name="counts")
+        fig=px.bar(data_frame=df, x=i, y="counts",color=i)
+        fig.update_layout(template='plotly_dark')
+        fig.update_xaxes(showgrid=False)
+        fig.update_yaxes(showgrid=False)
+        fig.show()
+        #fig.write_image(f"{letters[count]}count_{i}.jpg")
+        
+    
+    names=["carat","depth","table","price","x","y","z"]
+
+    for j in names:
+        count += 1
+        fig = px.box(data, y=j)
+        fig.update_layout(template='plotly_dark')
+        fig.update_xaxes(showgrid=False,zeroline=False)
+        fig.update_yaxes(showgrid=False,zeroline=False)
+        fig.show()
+        #fig.write_image(f"{letters[count]}box_{j}.jpg")
+
+        fig = ff.create_distplot([data[j].values],group_labels=[j])
+        fig.update_layout(template='plotly_dark')
+        fig.update_xaxes(showgrid=False,zeroline=False)
+        fig.update_yaxes(showgrid=False,zeroline=False)
+        fig.show()
+        #fig.write_image(f"{letters[count]}_dist_{j}.jpg")
+       
+    return data
+
+data_visualization()
